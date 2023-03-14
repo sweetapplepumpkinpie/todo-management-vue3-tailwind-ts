1336c7f2aa886d78fafa8d1ae08955473f6a4081
sweetapplepumpkinpie <sweetapplepumpkinpie@gmail.com>
2023-03-14 10:02:38 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## How to start
+
+`npm install`
+
+`npm run dev`
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue"
+</script>
+
+<template>
+  <div class="text-red-300">hello</div>
+</template>
+
+<style scoped></style>
